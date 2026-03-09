f005714af84111d060a9d43a8a1b064befbdc34d
Sayyed Neha Firdous <sayyedneha.firdous@gmail.com>
2026-03-10 00:07:04 +0100

    package and tsconfig files updated

--- src/server.ts
+++ src/server.ts
@@ -92,7 +92,7 @@ app.post("/generate", async (req: any, res: any) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
